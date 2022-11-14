5d138b56498f31be5cf2243e9eb2fe82b9b5c1dc
Jabbar <saj8180025653@gmail.com>
2022-11-14 22:44:48 +0530

    Add port and restarted again

--- app.js
+++ app.js
@@ -143,7 +143,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server has Started Successfully");
 });
